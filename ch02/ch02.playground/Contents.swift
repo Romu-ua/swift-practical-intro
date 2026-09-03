2c9808591cd4551b6c9b3402058f224bfe703c27
Romu-ua <ymmthrm15@fuji.waseda.jp>
2026-09-03 14:51:50 +0900

    ch02

---
+++ ch02/ch02.playground/Contents.swift
@@ -0,0 +1,15 @@
+var a: Int = 123
+let pi: Double = 3.14
+
+print(type(of: a))
+
+let original = [1, 2, 3]
+let doubled = original.map({value in value * 2})
+print(doubled)
+
+func someFunction() {
+    let a = "hoge"
+//    var a = 123 // 同じ変数名は宣言できない
+}
+
+
